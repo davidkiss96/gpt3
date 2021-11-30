879b9d2c3adaa4f41a2c5d9e02bd2904fbc0e0d9
David Kiss <davidkissdev@gmail.com>
2021-11-30 15:22:41 +0100

    fix(link): fix navbar link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Menu = () => (
       <a href="#wgpt3">What is GPT3?</a>
     </p>
     <p>
-      <a href="#possibilities">Open AI</a>
+      <a href="#possibility">Open AI</a>
     </p>
     <p>
       <a href="#features">Case Studies</a>
